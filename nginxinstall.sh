b3fd34f4c30023331fe60bc2f47a8fc7e5f0720c
booklist <lucktime@yeah.net>
2021-07-16 18:19:54 +0800

    Update nginxinstall.sh

--- nginxinstall.sh
+++ nginxinstall.sh
@@ -1,87 +1,138 @@
-#!/bin/bash
-#描述:Nginx安装脚本
-#Nginx版本：1.9.5
-echo    -e "\033[31m 搭建yum源 \033[0m"
-echo    -e "1.网络yum源"
-echo    -e "2.本地yum源"
-    read -p "请输入你的操作(1-2):" yum
-
-case $yum in
-1)
-yum install -y wget &> /dev/null
-rm -rf /etc/yum.repos.d/*
-wget -P /etc/yum.repos.d/ http://mirrors.aliyun.com/repo/Centos-7.repo &>/dev/null
-if [ $? -eq 0 ];then
-    echo    "yum源文件下载成功！！"
-else
-    echo    "yum源文件下载失败，请检查网络配置！！"
-    exit 1
-fi
-yum clean all &>/dev/null
-yum makecache &>/dev/null
-echo    "阿里yum源搭建完成！！！"
-;;
-2)
-rm -rf /etc/yum.repos.d/*
-cat >> /etc/yum.repos.d/yum.repo << eof
-[yum]
-name=yum
-baseurl=file:///mnt
-gpgcheck=0
-enabled=1
-eof
-yum clean all &>/dev/null
-yum makecache &>/dev/null
-echo    "本地yum源搭建完成！！"
-mount /dev/cdrom /mnt/
-cd /mnt/Packages/
-if [ $? -eq 0 ]; then
-    echo    "已将镜像挂载到系统！！"
-    cd /root/
-else
-    echo    "镜像挂载失败，请查看是否将镜像导入系统！！"
-    exit 2
-fi
-;;
-*)
-    echo    "请输入1或者2操作，输入错误，正在退出..."
-    exit    3
-esac
+#!/bin/sh
+############################################################################################
+# 说明：nginx一键部署
+# 作者：shuwoom
+# Email：shuwoom.wgc@gmail.com
+#############################################################################################
 
 
-echo    -e "\033[34m 安装Nginx \033[0m"
-sleep 3
-echo    "安装Nginx依赖包"
-nginx_gz=nginx-1.9.5.tar.gz
-nginx=nginx-1.9.5
-cd /root
-yum install -y gcc gcc-c++ autoconf automake zlib zlib-devel openssl openssl-devel  pcre pcre-devel make automake   &>/dev/null
-echo    "创建Nginx运行用户"
-groupadd www
-useradd -g www www -s /sbin/nologin
-tar xf $nginx_gz
-cd $nginx
-./configure --prefix=/usr/local/nginx --with-http_dav_module --with-http_stub_status_module --with-http_addition_module --with-http_sub_module --with-http_flv_module --with-http_mp4_module --with-pcre --with-http_ssl_module --with-http_gzip_static_module --user=www --group=www &>/dev/null
-if [ $? -eq 0 ];then
-    echo    "Nginx预编译完成，开始安装"
-else
-    echo    "Nginx预编译失败，请检查相关依赖包是否安装"
-    exit 4
-fi
-make &>/dev/null
-make install &>/dev/null
-if [ $? -eq 0 ];then
-    echo    "Nginx安装成功"
-else
-    echo    "Nginx安装失败"
-    exit 5
-fi
-ln -s /usr/local/nginx/sbin/nginx /usr/local/bin
-/usr/local/nginx/sbin/nginx
-netstat -anput | grep nginx &>/dev/null
-if [ $? -eq 0 ];then
-    echo    "Nginx启动成功"
-else
-    echo    "Nginx启动失败"
-    exit 6
-fi
+
+############################################################################################
+# Nginx参数配置
+#############################################################################################
+# NGINX安装包下载地址
+F_NGINX_PKG="http://nginx.org/download/nginx-1.15.9.tar.gz"
+# nginx安装路径
+D_NGINX_SERVICE="/user/share/nginx"
+# nginx日志保存路径
+D_NGINX_LOG="/var/log/nginx"
+# nginx web路径
+D_NGINX_WEB_ROOT="/user/share/nginx/html"
+# nginx进程用户属性
+V_NGINX_USER="nginx"
+# nginx监听端口
+V_NGINX_PORT=80
+
+
+[ -d ${D_NGINX_SERVICE} ] || mkdir -p ${D_NGINX_SERVICE}
+[ -d ${D_NGINX_LOG} ] || mkdir -p ${D_NGINX_LOG}
+
+
+
+############################################################################################
+# Nginx安装 
+# 版本：1.15.9
+#############################################################################################
+function install_nginx()
+{
+	local v_start_ts=$(date +%s)
+	local v_nginx_flag=$(nginx -v 2>&1 | awk 'NR==1{print substr($0,0,5)}')
+	if [[ "${v_nginx_flag}" == "nginx" ]]; then
+		echo "[INFO] nginx installed, ignore!"
+		return
+	else
+		echo "[INFO] Nginx not installed, begin to install Nginx!"
+	fi
+
+	echo "[INFO]============Start Nginx Installation=================="
+	if [[ ! -d "${D_TMP}" ]]; then
+		echo "[INFO] Create tmp dir"
+		mkdir -p ${D_TMP}
+	fi
+
+	wget ${F_NGINX_PKG}
+
+	tar -xf nginx-1.15.9.tar.gz
+
+	local d_nginx_pkg="${D_TMP}/nginx-1.15.9"
+
+	cd ${d_nginx_pkg}
+
+	# 安装编译环境
+	yum -y install gcc pcre pcre-devel zlib zlib-devel openssl openssl-devel
+
+	# 添加用户和组
+	local v_nginx_group=$(cat /etc/group|grep ${V_NGINX_USER})
+	if [[ -z "${v_nginx_group}" ]]; then
+		echo "[INFO] ${V_NGINX_USER}:${V_NGINX_USER} not exist, create it!"
+		groupadd ${V_NGINX_USER}
+		useradd -g ${V_NGINX_USER} ${V_NGINX_USER}
+	fi
+
+	# 配置
+	./configure \
+	--user=${V_NGINX_USER} \
+	--group=${V_NGINX_USER} \
+	--prefix=${D_NGINX_SERVICE} \
+	--with-http_ssl_module \
+	--with-http_stub_status_module \
+	--with-http_realip_module \
+	--with-threads
+
+	# 编译安装
+	make && make install
+
+	local v_nginx_flag=$(${D_NGINX_SERVICE}/sbin/nginx -v 2>&1 | awk 'NR==1{print substr($0,0,5)}')
+	if [[ "${v_nginx_flag}" == "nginx" ]]; then
+		echo "[INFO] Nginx installed successfully!"
+	else
+		echo "[ERROR] Nginx installed failed!"
+		exit 0
+	fi
+
+	if [[ ! -e "/usr/bin/nginx" ]]; then
+		ln -s ${D_NGINX_SERVICE}/sbin/nginx /usr/bin/nginx
+	fi
+
+	if [[ -e " ${D_NGINX_SERVICE}/conf/nginx.conf" ]]; then
+		mv  ${D_NGINX_SERVICE}/conf/nginx.conf ${D_NGINX_SERVICE}/conf/nginx.conf.bak
+	fi
+	cp nginx.conf  ${D_NGINX_SERVICE}/conf/nginx.conf
+	sed -i "s#user  nginx;#user  ${V_NGINX_USER};#"
+	sed -i "s#error_log  logs\/error.log  error;#error_log  ${D_NGINX_LOG}\/error.log  error;#" ${D_NGINX_SERVICE}/conf/nginx.conf
+	sed -i "s#root         \/user\/share\/nginx\/html;#root         ${D_NGINX_WEB_ROOT};#" ${D_NGINX_SERVICE}/conf/nginx.conf
+	sed -i "s#listen       80;#listen       ${V_NGINX_PORT};#" ${D_NGINX_SERVICE}/conf/nginx.conf
+	
+	# 添加开机启动
+	cp nginx /etc/init.d/nginx
+	sed -i "s#nginx=\"\/usr\/local\/nginx\/sbin\/nginx\"#nginx=\"${D_NGINX_SERVICE}\/sbin/nginx\"#" /etc/init.d/nginx
+	sed -i "s#NGINX_CONF_FILE=\"\/usr\/local\/nginx\/conf\/nginx.conf\"#NGINX_CONF_FILE=\"${D_NGINX_SERVICE}\/conf\/nginx.conf\"#"  /etc/init.d/nginx
+	
+	chmod +x /etc/init.d/nginx
+	chkconfig --add /etc/init.d/nginx
+	chkconfig nginx on
+	service nginx start
+
+	# 创建web访问目录
+	if [[ ! -d "${D_NGINX_WEB_ROOT}" ]]; then
+		mkdir -p ${D_NGINX_WEB_ROOT}
+	fi
+
+	chown -R ${V_NGINX_USER}:${V_NGINX_USER} ${D_NGINX_WEB_ROOT}
+
+	# 检测是否启动成功
+	local v_nginx_cnt=$(ps -ef|grep nginx|grep -v grep|wc -l)
+	if [[ ${v_nginx_cnt} -gt 0 ]]; then
+		echo "[INFO] Nginx start successfully!"
+	else
+		echo "[ERROR] Nginx start failed!"
+		exit 0
+	fi
+
+	local v_end_ts=$(date +%s)
+	local v_cost_ts=`expr ${v_end_ts} - ${v_start_ts}`
+	echo "[INFO] Nginx Total cost: ${v_cost_ts}"
+	echo "[INFO]============End of Nginx Installation=================="
+}
+
+install_nginx
